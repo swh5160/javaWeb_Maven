cfa4b4ff72406b2b927aaddc49a690137ca1c598
“swh5160” <1640070547@qq.com>
2023-03-18 14:28:09 +0800

    Initial commit

---
+++ mvn_pro01/src/main/java/com/shizin/App.java
@@ -0,0 +1,13 @@
+package com.shizin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mvn_pro02/src/main/java/com/shiziana/Main.java
@@ -0,0 +1,21 @@
+package com.shiziana;
+
+import org.junit.Test;
+
+/**
+ * @author 施子安
+ * @version 1.0
+ * @date ${DATE} ${TIME}
+ * @Description ${Description}
+ */
+public class Main {
+    /*public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }*/
+
+
+    @Test
+    public void test(){
+        System.out.println("hello world");
+    }
+}
